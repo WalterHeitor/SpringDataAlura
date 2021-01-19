d573ecbaf06e827a1234c7b6d3db222aeecda411
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-01-19 18:21:06 -0300

    Update SpringDataApplication.java

--- spring-data/spring-data/src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ spring-data/spring-data/src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -3,6 +3,7 @@ package br.com.alura.spring.data;
 import br.com.alura.spring.data.service.CrudCargoService;
 import br.com.alura.spring.data.service.CrudFuncionarioService;
 import br.com.alura.spring.data.service.CrudUnidadeTrabalhoService;
+import br.com.alura.spring.data.service.RelatoriosService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,11 +22,13 @@ public class SpringDataApplication implements CommandLineRunner {
 	private final CrudCargoService crudCargoService;
 	private final CrudFuncionarioService funcionarioService;
 	private final CrudUnidadeTrabalhoService unidadeTrabalhoService;
+	private final RelatoriosService relatoriosService;
 
-	public SpringDataApplication(CrudCargoService crudCargoService, CrudFuncionarioService funcionarioService, CrudUnidadeTrabalhoService unidadeTrabalhoService) {
+	public SpringDataApplication(CrudCargoService crudCargoService, CrudFuncionarioService funcionarioService, CrudUnidadeTrabalhoService unidadeTrabalhoService, RelatoriosService relatoriosService) {
 		this.crudCargoService = crudCargoService;
 		this.funcionarioService = funcionarioService;
 		this.unidadeTrabalhoService = unidadeTrabalhoService;
+		this.relatoriosService = relatoriosService;
 	}
 
 	@Override
@@ -38,20 +41,24 @@ public class SpringDataApplication implements CommandLineRunner {
 			System.out.println("| Qual açao você quer executar  |");
 			System.out.println("| 		0 - Sair 				|");
 			System.out.println("| 		1 - Funcionário: 		|");
-			System.out.println("| 		1 - Cargo: 				|");
-			System.out.println("| 		1 - Unidade: 			|");
+			System.out.println("| 		2 - Cargo: 				|");
+			System.out.println("| 		3 - Unidade: 			|");
 			System.out.println("--------------------------------");
 			Integer function = scanner.nextInt();
 
 			switch (function) {
 				case 1:
-					crudCargoService.inicial(scanner);
+					funcionarioService.inicial(scanner);
 					break;
 				case 2:
-					funcionarioService.inicial(scanner);
+					crudCargoService.inicial(scanner);
+
 					break;
 				case 3:
 					unidadeTrabalhoService.inicial(scanner);
+					break;
+				case 4:
+					relatoriosService.inicial(scanner);
 					break;
 				default:
 					System.out.println("Finalizando");
@@ -59,8 +66,5 @@ public class SpringDataApplication implements CommandLineRunner {
 					break;
 			}
 		}
-
-
-
 	}
 }
