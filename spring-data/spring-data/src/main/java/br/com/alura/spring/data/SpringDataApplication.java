ceb8e10c9f5e383226fe00eb19f53951bcfbc694
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-01-18 23:23:41 -0300

    Update SpringDataApplication.java

--- spring-data/spring-data/src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ spring-data/spring-data/src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -1,13 +1,30 @@
 package br.com.alura.spring.data;
 
+import br.com.alura.spring.data.orm.Cargo;
+import br.com.alura.spring.data.repository.CargoRepositoryCrud;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringDataApplication {
+public class SpringDataApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
 	}
 
+	private final CargoRepositoryCrud repositoryCrud;
+
+	public SpringDataApplication(CargoRepositoryCrud repositoryCrud) {
+		this.repositoryCrud = repositoryCrud;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Cargo cargo = new Cargo();
+		cargo.setDescricao("DESENVOLVEDOR DE SOFTWARE");
+		repositoryCrud.save(cargo);
+
+	}
 }
