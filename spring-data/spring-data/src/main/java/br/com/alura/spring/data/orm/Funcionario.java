eed0d3a80730a8aee9f16045f433c3b40523c12f
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-01-19 13:38:30 -0300

    Update Funcionario.java

--- spring-data/spring-data/src/main/java/br/com/alura/spring/data/orm/Funcionario.java
+++ spring-data/spring-data/src/main/java/br/com/alura/spring/data/orm/Funcionario.java
@@ -1,4 +1,84 @@
 package br.com.alura.spring.data.orm;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+@Entity
 public class Funcionario {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    private String cpf;
+    private Double salario;
+    private LocalDate dataContratacao;
+    @ManyToOne
+    @JoinColumn(name = "cargo_id", nullable = false)
+    private Cargo cargo;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "funcionarios_unidades",joinColumns = {
+            @JoinColumn(name = "fk_funcionario") },
+    inverseJoinColumns = {
+            @JoinColumn(name = "fk_unidade")    })
+    private List<UnidadeTrabalho> unidadeTrabalhos;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+
+    public LocalDate getDataContratacao() {
+        return dataContratacao;
+    }
+
+    public void setDataCotratacao(LocalDate dataCotratacao) {
+        this.dataContratacao = dataCotratacao;
+    }
+
+    public Cargo getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(Cargo cargo) {
+        this.cargo = cargo;
+    }
+
+    public void setDataContratacao(LocalDate dataContratacao) {
+        this.dataContratacao = dataContratacao;
+    }
+
+    public List<UnidadeTrabalho> getUnidadeTrabalhos() {
+        return unidadeTrabalhos;
+    }
+
+    public void setUnidadeTrabalhos(List<UnidadeTrabalho> unidadeTrabalhos) {
+        this.unidadeTrabalhos = unidadeTrabalhos;
+    }
 }
