2602ae68540a893060215c5a7510cd94a12d1f03
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-01-19 13:38:26 -0300

    Update Cargo.java

--- spring-data/spring-data/src/main/java/br/com/alura/spring/data/orm/Cargo.java
+++ spring-data/spring-data/src/main/java/br/com/alura/spring/data/orm/Cargo.java
@@ -5,28 +5,40 @@ import java.util.List;
 
 @Entity
 public class Cargo {
-	
-	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
-	private String descricao;
-	@OneToMany(mappedBy = "cargo")
-	private List<Funcionario> funcionarios;
 
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getDescricao() {
-		return descricao;
-	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String descricao;
+    @OneToMany(mappedBy = "cargo")
+    private List<Funcionario> funcionarios;
 
-	@Override
-	public String toString() {
-		return "Cargo{" +" id= " + id +	" , descricao=' " + descricao + '\'' + '}';
-	}
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Funcionario> getFuncionarios() {
+        return funcionarios;
+    }
+
+    public void setFuncionarios(List<Funcionario> funcionarios) {
+        this.funcionarios = funcionarios;
+    }
+
+    @Override
+    public String toString() {
+        return "Cargo{" + " id= " + id + " , descricao=' " + descricao + '\'' + '}';
+    }
 }
