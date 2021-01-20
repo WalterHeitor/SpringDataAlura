bc067f840cb16853323d33686560430b14dda683
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-01-20 00:37:29 -0300

    Update Funcionario.java

--- spring-data/spring-data/src/main/java/br/com/alura/spring/data/orm/Funcionario.java
+++ spring-data/spring-data/src/main/java/br/com/alura/spring/data/orm/Funcionario.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
 @Entity
+@Table(name = "funcionario")
 public class Funcionario {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
