b6a488d463d9dc72696ae02f7de11fd18e123f78
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-01-20 00:37:56 -0300

    Update RelatoriosService.java

--- spring-data/spring-data/src/main/java/br/com/alura/spring/data/service/RelatoriosService.java
+++ spring-data/spring-data/src/main/java/br/com/alura/spring/data/service/RelatoriosService.java
@@ -2,8 +2,11 @@ package br.com.alura.spring.data.service;
 
 import br.com.alura.spring.data.orm.Funcionario;
 import br.com.alura.spring.data.repository.FuncionarioRepositoryCrud;
+import org.springframework.format.datetime.DateFormatter;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Scanner;
 
@@ -11,6 +14,7 @@ import java.util.Scanner;
 public class RelatoriosService {
 
     private Boolean system = true;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     private FuncionarioRepositoryCrud funcionarioRepositoryCrud;
 
@@ -23,17 +27,24 @@ public class RelatoriosService {
 
     public void inicial(Scanner scanner){
         while (system){
-            System.out.println("--------------------------------------");
-            System.out.println("| Qual ação deseja executar?          |");
-            System.out.println("|   0 - Sair                          |");
-            System.out.println("|   1 - Busca funcionario por nome    |");
-            System.out.println("--------------------------------------");
+            System.out.println("-------------------------------------------------------------");
+            System.out.println("| Qual ação deseja executar?                                 |");
+            System.out.println("| 0 - Sair                                                   |");
+            System.out.println("| 1 - Busca funcionario por nome                             |");
+            System.out.println("| 2 - Busca funcionario por Salario maior e data contratação |");
+            System.out.println("| 3 - Busca funcionario por data Contratação                 |");
+            System.out.println("-------------------------------------------------------------");
             int action = scanner.nextInt();
             switch (action){
                 case 1:
                     buscaFuncionarioPorNnome(scanner);
                     break;
-
+                case 2:
+                    buscarFuncionarioNomeSalarioMaiorData(scanner);
+                    break;
+                case 3:
+                    buscarFuncionarioDataContratacao(scanner);
+                    break;
                 case 0:
                 default:
                     system = false;
@@ -54,8 +65,18 @@ public class RelatoriosService {
         String nomeFuncionario = scanner.nextLine();
         System.out.println("Qual data deseja pesquisar?");
         String dataFuncionario = scanner.nextLine();
+        LocalDateTime localDateTime = LocalDateTime.parse(dataFuncionario, formatter);
         System.out.println("Qual salario deseja perquisar");
         Double salario = scanner.nextDouble();
-        funcionarioRepositoryCrud.findNomeSalarioMaiorDataContratacao(nomeFuncionario, salario, dataFuncionario);
+        List<Funcionario>list = funcionarioRepositoryCrud.findNomeSalarioMaiorDataContratacao(nomeFuncionario, salario, localDateTime);
+        list.forEach(System.out::println);
+    }
+
+    private void buscarFuncionarioDataContratacao(Scanner scanner){
+        System.out.println("Qual data deseja pesquisar?");
+        String dataFuncionario = scanner.nextLine();
+        LocalDateTime localDateTime = LocalDateTime.parse(dataFuncionario, formatter);
+        List<Funcionario>list = funcionarioRepositoryCrud.findDdataContracaoMaior(localDateTime);
+        list.forEach(System.out::println);
     }
 }
